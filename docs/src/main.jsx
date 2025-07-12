0823c47983b196e266ee1f4d04ee999db5d6ea2b
Mario Silva <mario8988silva@gmail.com>
2025-07-12 11:58:31 +0100

    modulos css

--- docs/src/main.jsx
+++ docs/src/main.jsx
@@ -3,7 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 import './assets/reset.css';
-import './assets/style.css';
+import './assets/variables.css';
+import './assets/globals.css';
+
+import './assets/header.css';
+import './assets/banner.css';
+import './assets/mainFilters.css';
+import './assets/mainResults.css';
+import './assets/projectCard.css';
+import './assets/servicesAndPath.css';
+import './assets/valuesAndMethods.css';
+import './assets/footer.css';
 
 import App from './App.jsx'
 
