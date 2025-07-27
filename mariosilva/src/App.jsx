6a9f82017dcdac76273a2b8cfb3bf86524ed30f1
Mario Silva <mario8988silva@gmail.com>
2025-07-27 15:12:23 +0100

    scroll to top, utils

--- mariosilva/src/App.jsx
+++ mariosilva/src/App.jsx
@@ -1,52 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import 'normalize.css'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "normalize.css";
+import "./App.css";
 
 // routing:
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation } from "react-router-dom";
 
-import ProjectPage from './components/pages/ProjectPage';
-import ErrorPage from './components/pages/ErrorPage';
+import ProjectPage from "./components/pages/ProjectPage";
+import ErrorPage from "./components/pages/ErrorPage";
 
 // sections:
-import Header from './components/Header';
-import Banner from './components/Banner';
+import Header from "./components/Header";
+import Banner from "./components/Banner";
 
-import ProjectsMain from './components/ProjectsMain';
+import ProjectsMain from "./components/ProjectsMain";
 
 // falta toogle para filtros
-import ProjectsFilters from './components/ProjectsFilters';
-import ProjectsResults from './components/ProjectsResults';
+import ProjectsFilters from "./components/ProjectsFilters";
+import ProjectsResults from "./components/ProjectsResults";
 
-import About from './components/About';
-import Values from './components/Values';
-import Footer from './components/Footer';
+import About from "./components/About";
+import Values from "./components/Values";
+import Footer from "./components/Footer";
 
 // componentes:
-import ProjectCard from './components/ProjectCard'
+import ProjectCard from "./components/ProjectCard";
 
 // utils:
-import { FilterProvider } from './app/store';
-
+import { FilterProvider } from "./app/store";
+import ScrollToTop from "./utils/ScrollToTop";
 
 function App() {
-
   const location = useLocation();
 
   const isMainPage = location.pathname === "/";
   const isProjectPage = location.pathname.startsWith("/projects/");
-  /*const isErrorPage = location.pathname !== '/' && !location.pathname.startsWith('/projects/');*/
-/*
-  const showLayout = isMainPage || isProjectPage;
-  */
 
   const isErrorPage = !isMainPage && !isProjectPage;
-const showLayout = !isErrorPage;
+  const showLayout = !isErrorPage;
 
   return (
     <>
+      <ScrollToTop />
+
       {/* conteúdo comum */}
       {showLayout && <Header />}
 
@@ -65,23 +62,14 @@ const showLayout = !isErrorPage;
         />
 
         {/* project page */}
-        <Route
-          path="/projects/:slug"
-          element={<ProjectPage />}
-        />
+        <Route path="/projects/:slug" element={<ProjectPage />} />
 
         <Route path="*" element={<ErrorPage />} />
-
       </Routes>
 
       {/* conteúdo comum */}
       {showLayout && <Footer />}
-
-      
-
-      
     </>
-    
   );
 }
 
